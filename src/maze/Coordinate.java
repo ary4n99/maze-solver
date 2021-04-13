0d68f040a09db41c6cbc5304b828f1ee93c01500
Aryan Agrawal <ary4n99@gmail.com>
2021-04-13 17:31:54 +0100

    Add Coordinate methods and variables

---
+++ src/maze/Coordinate.java
@@ -0,0 +1,23 @@
+package maze;
+
+public class Coordinate {
+
+    private int x;
+
+    private int y;
+
+    public Coordinate(int xIn, int yIn) {
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public String toString() {
+    }
+    
+}
