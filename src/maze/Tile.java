19c0c2108ff3689097ee612f57033e6015c99e24
Aryan Agrawal <ary4n99@gmail.com>
2021-04-13 22:19:26 +0100

    Implements methods for Tile class

--- src/maze/Tile.java
+++ src/maze/Tile.java
@@ -5,9 +5,13 @@ public class Tile {
     private Type type;
 
     private Tile(Type typeIn) {
+        type = typeIn;
     }
 
     protected static Tile fromChar(char charIn) {
+        return charIn == '#' ? new Tile(Type.WALL)
+                : charIn == '.' ? new Tile(Type.CORRIDOR)
+                        : charIn == 'e' ? new Tile(Type.ENTRANCE) : charIn == 'x' ? new Tile(Type.EXIT) : null;
     }
 
     public Type getType() {
@@ -15,9 +19,12 @@ public class Tile {
     }
 
     public boolean isNavigable() {
+        return type != Type.WALL ? true : false;
     }
 
     public String toString() {
+        return type == Type.WALL ? "#"
+                : type == Type.CORRIDOR ? "." : type == Type.ENTRANCE ? "e" : type == Type.EXIT ? "x" : null;
     }
 
 }
