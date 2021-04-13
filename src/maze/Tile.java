702122e54b53346764b176ca862d07ab0f697790
Aryan Agrawal <ary4n99@gmail.com>
2021-04-13 14:52:49 +0100

    Add Tile methods and variables

---
+++ src/maze/Tile.java
@@ -0,0 +1,23 @@
+package maze;
+
+public class Tile {
+
+    private Type type;
+
+    private Tile(Type typeIn) {
+    }
+
+    protected static Tile fromChar(char charIn) {
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public boolean isNavigable() {
+    }
+
+    public String toString() {
+    }
+
+}
