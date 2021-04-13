0406c405c93a015c67ad0dff9073a2019e0d5fac
Aryan Agrawal <ary4n99@gmail.com>
2021-04-13 13:37:51 +0100

    Implement initial maze.routing methods

---
+++ src/maze/routing/RouteFinder.java
@@ -0,0 +1,40 @@
+package maze.routing;
+
+import maze.Tile;
+import maze.Maze;
+import java.util.List;
+import java.util.Stack;
+
+public class RouteFinder {
+
+    private Maze maze;
+
+    private Stack<Tile> route;
+
+    private boolean finished;
+
+    public RouteFinder(Maze mazeIn) {
+    }
+
+    public Maze getMaze() {
+    }
+
+    public List<Tile> getRoute() {
+    }
+
+    public boolean isFinished() {
+    }
+
+    public static RouteFinder load(String stringIn) {
+    }
+
+    public void save(String stringIn) {
+    }
+
+    public boolean step() {
+    }
+
+    public String toString() {
+    }
+
+}
