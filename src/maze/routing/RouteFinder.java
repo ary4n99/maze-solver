cc6a9f00b77d83df1e5e259b7cda88f8207c6e90
Aryan Agrawal <ary4n99@gmail.com>
2021-04-16 12:02:47 +0100

    Completes some RouteFinder methods

--- src/maze/routing/RouteFinder.java
+++ src/maze/routing/RouteFinder.java
@@ -14,15 +14,22 @@ public class RouteFinder {
     private boolean finished;
 
     public RouteFinder(Maze mazeIn) {
+        maze = mazeIn;
+        route = new Stack<Tile>();
+        finished = false;
     }
 
     public Maze getMaze() {
+        return maze;
     }
 
     public List<Tile> getRoute() {
+        return route;
     }
 
     public boolean isFinished() {
+        return finished;
+
     }
 
     public static RouteFinder load(String stringIn) {
@@ -35,6 +42,14 @@ public class RouteFinder {
     }
 
     public String toString() {
+        String string = "";
+        List<List<Tile>> mazeTiles = maze.getTiles();
+        for (List<Tile> row : mazeTiles) {
+            for (Tile tile : row) {
+                string += route.contains(tile) ? "*" : tile.toString();
+            }
+            string += "\n";
+        }
+        return string;
     }
-
 }
