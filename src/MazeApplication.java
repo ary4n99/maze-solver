e95296a245a4119431ac3b7c25911c474919a93f
Aryan Agrawal <ary4n99@gmail.com>
2021-04-16 11:48:45 +0100

    Adds basic buttons to MazeApplication

--- src/MazeApplication.java
+++ src/MazeApplication.java
@@ -1,5 +1,11 @@
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 
 public class MazeApplication extends Application {
     public static void main(String[] args) {
@@ -7,5 +13,18 @@ public class MazeApplication extends Application {
     }
 
     public void start(Stage stage) {
+        stage.setTitle("Maze Solver");
+        BorderPane root = new BorderPane();
+        Label title = new Label("Maze Solver");
+        Button button1 = new Button("Button 1");
+        Button button2 = new Button("Button 2");
+
+        VBox vbox = new VBox(title, button1, button2);
+        vbox.setAlignment(Pos.CENTER);
+        root.setCenter(vbox);
+
+        Scene scene = new Scene(root, 600, 600);
+        stage.setScene(scene);
+        stage.show();
     }
 }
