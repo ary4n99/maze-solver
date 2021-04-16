c491bb0c3b3f1a58657e03fc1bd7b4268b99eab0
Aryan Agrawal <ary4n99@gmail.com>
2021-04-16 22:35:03 +0100

    Implements main RouteFinder methods

--- src/maze/routing/RouteFinder.java
+++ src/maze/routing/RouteFinder.java
@@ -13,6 +13,8 @@ public class RouteFinder {
 
     private boolean finished;
 
+    private List<Tile> visited;
+
     public RouteFinder(Maze mazeIn) {
         maze = mazeIn;
         route = new Stack<Tile>();
@@ -33,12 +35,48 @@ public class RouteFinder {
     }
 
     public static RouteFinder load(String stringIn) {
+        RouteFinder routeFinder;
+        try (FileInputStream file = new FileInputStream(stringIn);
+                ObjectInputStream stream = new ObjectInputStream(file);) {
+            return (RouteFinder) stream.readObject();
+        } catch (IOException e) {
+            throw new IOException("Problem loading file.");
+        }
     }
 
     public void save(String stringIn) {
+        try (FileOutputStream file = new FileOutputStream(stringIn);
+                ObjectOutputStream stream = new ObjectOutputStream(file);) {
+            stream.writeObject(this);
+        } catch (IOException e) {
+            throw new IOException("Problem saving file.");
+        }
     }
 
     public boolean step() {
+        if (!finished) {
+            if (!route.contains(maze.getEntrance()) && !visited.contains(maze.getEntrance())) {
+                route.push(maze.getEntrance());
+                visited.add(maze.getEntrance());
+            } else {
+                if (route.size() > 0) {
+                    for (Maze.Direction direction : Maze.Direction.values()) {
+                        Tile tile = maze.getAdjacentTile(route.peek(), direction);
+                        if (tile != null && tile.isNavigable() && !visited.contains(tile)) {
+                            visited.add(tile);
+                            route.push(tile);
+                            return finished = tile.toString().equals("x");
+                        }
+                    }
+                    route.pop();
+                } else {
+                    throw new NoRouteFoundException("No route found.");
+                }
+            }
+        } else if (!visited.contains(maze.getExit())) {
+            visited.add(maze.getExit());
+        }
+        return finished;
     }
 
     public String toString() {
